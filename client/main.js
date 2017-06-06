eea4e6d24baf9739e62d7c0bf06708a6e3ae6246
Troll Kim <ikim0106@hotmail.com>
2017-06-06 10:12:31 +0900

    added some html elements

--- client/main.js
+++ client/main.js
@@ -3,10 +3,23 @@ import { ReactiveVar } from 'meteor/reactive-var';
 
 import './main.html';
 
+Todos = new Mongo.Collection('todos');
+
 Template.body.helpers({
-  todos:[
-    {title: "make trump memes"},
-    {title: "do a bit of homework"},
-    {title: "continue making trump memes"}
-  ]
+  todos:function(){
+    return Todos.find();
+  }
+});
+
+Template.body.events({
+  'submit .todo-list': function(event){
+    var title = event.target.title.value;
+
+    Todos.insert({
+      title: title, 
+      createdAt: new Date()
+    });
+    event.target.title.value= "";
+    return false; 
+  }
 });
